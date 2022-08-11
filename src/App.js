c8fc06a7d9d416baf6b6f433b08eedfccb6661a9
sss <sshoony95@gmail.com>
2022-08-11 15:24:42 -0400

    Change to class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
-function App() {
-  return (
-    <div>
-    </div>
-  );
+import React from "react";
+class App extends React.Component {
+  render() {
+      return (
+        <div>
+        </div>
+      );
+    }
 }
 
 export default App;
